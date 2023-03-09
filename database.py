54c1f8342cb29e8543a96b793d26c71815ea687e
MissBlumarine <miss_blumarine01@mail.ru>
2023-03-09 23:16:14 +0300

    + crud.py + schemas.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import Column, String, Integer
+
 from fastapi import FastAPI
 
 # строка подключения к БД
@@ -12,17 +12,15 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_software.db"
 # позволяет в рамках одного запроса к БД использовать больше, чем один поток (для FastAPI)
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
+
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+
 # базовый класс
 Base = declarative_base()
 
 
-# модель, объекты которой будут записываться в БД
-class Software(Base):
-    __tablename__ = "software"
-
-    id = Column(Integer, primary_key=True, index=True)
-    software = Column(String)
-    version = Column(String)
 
 
-SessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+
--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from sqlalchemy.orm import Session
 from fastapi.responses import JSONResponse, FileResponse
 from database import *
 
-
 # создаем таблицу
 Base.metadata.create_all(bind=engine)
 
@@ -17,32 +16,40 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
 
 @app.get("/versions")
 async def get_all_software(db: Session = Depends(get_db)):
     return db.query(Software).all()
 
+
 @app.get("/versions/{id}")
 async def get_software_by_id(id, db: Session = Depends(get_db)):
     software = db.query(Software).filter(Software.id == id).first()
     if software == None:
-        return JSONResponse(status_code=404, content={'внимание': 'Такого продукта нет'})
+        return JSONResponse(status_code=404, content={'внимание': f'Software_продукта c id={id} не существует'})
     return software
 
 
-# @app.post("/version")
-# def create_software(data=Body(), db: Session = Depends(get_db)):
-#     software = Software(software=data['software'], version=data['version'])
-#     db.add(software)
+
+@app.post("/version")
+async def create_software(data= Body(), db: Session = Depends(get_db)):
+    product = Software(software=data["software"], version=data["version"])
+    db.add(product)
+    db.commit()
+    db.refresh(product)
+    return product
+#
+#
+# @app.patch("/users/patch")
+# async def patch_software(data = Body(), db: Session = Depends(get_db)):
+#     product = db.query(Software).filter(Software.id == data["id"]).first()
+#     if product == None:
+#         return JSONResponse(status_code=404, content={'внимание': f'Software_продукта c id={id} не существует'})
+#     product.software = data["software"]
+#     product.version = data["version"]
 #     db.commit()
-#     db.refresh(software)
-#     return software
-
-
+#     db.refresh(product)
+#     return product
 
 
 # @app.get("/version")
